70a9fd88642cfe88ac31d3b91143fb9e26461587
Scrobwofl <scrobwofl@gmail.com>
2024-01-18 23:38:38 +0000

    Further work

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const upgradeImages = [
 const upgradeContainer = document.querySelector("#upgrades-container");
 const clickableImage = document.querySelector("#clickable-image");
 const scoreContainer = document.querySelector("#score-container");
+const gameTimeContainer = document.querySelector("#gme-time");
+const outerCountdownBar = document.querySelector("#inner-bar");
 const innerCountdownBar = document.querySelector("#inner-bar");
 const reducerContainer = document.querySelector("#reducer-container");
 const multiplierContainer = document.querySelector("#multiplier-container");
@@ -33,26 +35,24 @@ function createImageNode(imageLocation) {
 // Upgrade image array loop through
 upgradeImages.forEach((img, count) => {
   upgradeContainer.appendChild(createImageNode(img));
-  count = count * 100;
+  // count = count * 100;
 });
 
 // Declaring a starting object structure
 let userObject = {
-  gameTime: 1000,
   currentScore: 10,
-  reducerValue: 1,
   multiplier: 1,
 };
 
 // Initial Variables
-let gameTime = userObject.gameTime;
+let gameTime = 100;
 let currentScore = userObject.currentScore;
-let reducerValue = userObject.reducerValue;
 let multiplierValue = userObject.multiplier;
 
 // Click event listener
 clickableImage.addEventListener("click", () => {
   currentScore = currentScore + multiplierValue;
+  scoreContainer.innerHTML = `<p> Score: ${currentScore}</p>`;
   clickableImage.classList.add("scaleIt");
   addEventListener(
     "animationend",
@@ -67,23 +67,17 @@ clickableImage.addEventListener("click", () => {
 
 // function scoreEvaluator(x) {
 //   if (x > 0 && x <= 99) {
-//     reducerValue = reducerValue * 1;
 //     multiplierValue = multiplierValue * 1;
 //   } else if (x >= 100 && x <= 199) {
-//     reducerValue = reducerValue * 2;
-//     multiplierValue = multiplierValue + 1;
+//     multiplierValue = multiplierValue + 2;
 //   } else if (x >= 200 && x <= 299) {
-//     reducerValue = reducerValue * 2;
-//     multiplierValue = multiplierValue + 1;
+//     multiplierValue = multiplierValue + 3;
 //   } else if (x >= 300 && x <= 399) {
-//     reducerValue = reducerValue * 2;
-//     multiplierValue = multiplierValue + 1;
+//     multiplierValue = multiplierValue + 4;
 //   } else if (x >= 400 && x <= 499) {
-//     reducerValue = reducerValue * 2;
-//     multiplierValue = multiplierValue + 1;
+//     multiplierValue = multiplierValue + 5;
 //   } else if (x >= 500 && x <= 599) {
-//     reducerValue = reducerValue * 2;
-//     multiplierValue = multiplierValue + 1;
+//     multiplierValue = multiplierValue + 6;
 //   } else if (x >= 600) {
 //     alert("You won the public vote!");
 //   } else if (x <= 0) {
@@ -91,25 +85,25 @@ clickableImage.addEventListener("click", () => {
 //   }
 // }
 
-function gameEvaluator(x) {
-  if (x == 0) {
-    alert("You have faced a parliamentary defeat you weasal!");
-  }
-}
+// function gameEvaluator(x) {
+//   if (x == 0) {
+//     alert("You have faced a parliamentary defeat you weasal!");
+//   }
+// }
 
 // Game Countdown
-setTimer(() => {
-  currentScore = currentScore - reducerValue;
-  gameTime = gameTime - 1;
-}, 10000);
+// setTimer(() => {
+//   currentScore = currentScore - 1;
+// }, 10000);
 
 // Timer Updater
 setInterval(() => {
-  gameEvaluator(currentScore);
-  innerCountdownBar.innerHTML = `<p>${currentScore}</p>`;
-  reducerContainer.innerHTML = `<p>Crowd Anger: ${reducerValue}</p>`;
-  multiplierContainer.innerHTML = `<p>Public Apathy: ${multiplierValue}</p>`;
-  console.log(timeRemaining);
+  // gameEvaluator(currentScore);
+  gameTime = gameTime - 1;
+  gameTimeContainer.textContent = `${gameTime}`;
+  innerCountdownBar.setAttribute("style", `width:${gameTime}%`);
+  // reducerContainer.innerHTML = `<p>Crowd Anger: ${reducerValue}</p>`;
+  // multiplierContainer.innerHTML = `<p>Public Apathy: ${multiplierValue}</p>`;
 }, 1000);
 // If score is above level do this..
 // - Update image
