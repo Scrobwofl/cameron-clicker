e4819c6420f48c3687211f6d01be1a8bcfb4a36d
Scrobwofl <scrobwofl@gmail.com>
2024-01-19 11:59:31 +0000

    Add sounds but broken, going for lunch

--- app.js
+++ app.js
@@ -9,20 +9,37 @@ const upgradeImages = [
   "./images/nigel-farage.png",
   "./images/theresa-may.png",
 ];
+// Sound Imports
+const sound1 = document.querySelector("#hit-sound-1");
+const sound2 = document.querySelector("#hit-sound-2");
+const sound3 = document.querySelector("#hit-sound-3");
+const sound4 = document.querySelector("#hit-sound-4");
+const sound5 = document.querySelector("#hit-sound-5");
+const sound6 = document.querySelector("#hit-sound-6");
+const sound7 = document.querySelector("#hit-sound-7");
+const sound8 = document.querySelector("#hit-sound-8");
+const rabble = document.querySelector("#rabble");
+
+const hitSoundsArray = [
+  sound1,
+  sound2,
+  sound3,
+  sound4,
+  sound5,
+  sound6,
+  sound7,
+  sound8,
+];
 
 // Page Element Selectors
 const upgradeContainer = document.querySelector("#upgrades-container");
 const clickableImage = document.querySelector("#clickable-image");
 const scoreContainer = document.querySelector("#score-container");
-const gameTimeContainer = document.querySelector("#gme-time");
+const gameTimeContainer = document.querySelector("#game-time");
 const outerCountdownBar = document.querySelector("#inner-bar");
 const innerCountdownBar = document.querySelector("#inner-bar");
 const reducerContainer = document.querySelector("#reducer-container");
 const multiplierContainer = document.querySelector("#multiplier-container");
-
-// if (localStorage) {
-//   let initialScore = 100;
-// }
 
 // Utility function - Image Appender
 function createImageNode(imageLocation) {
@@ -32,7 +49,7 @@ function createImageNode(imageLocation) {
   return img;
 }
 
-// Upgrade image array loop through
+// Upgrade images - array loop and append
 upgradeImages.forEach((img, count) => {
   upgradeContainer.appendChild(createImageNode(img));
   // count = count * 100;
@@ -49,65 +66,93 @@ let gameTime = 100;
 let currentScore = userObject.currentScore;
 let multiplierValue = userObject.multiplier;
 
+// Utility Function to generate random values
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Play a random sound from the
+function playSound() {
+  let i = getRandomNumber(0, hitSoundsArray.length);
+  hitSoundsArray[i].play();
+}
+
 // Click event listener
 clickableImage.addEventListener("click", () => {
+  // Play random sound
+  playSound();
   currentScore = currentScore + multiplierValue;
-  scoreContainer.innerHTML = `<p> Score: ${currentScore}</p>`;
+  scoreContainer.innerHTML = `<p>Score: ${currentScore}</p>`;
+
+  // Randomly rotate the head on click
+  const randomRotation = getRandomNumber(-20, 20);
+  clickableImage.style.transform = `rotate(${randomRotation}deg)`;
+
+  //Reset the transform rotate after 1 sec.
+  setTimeout(() => {
+    clickableImage.style.transform = "rotate(0deg)";
+  }, 1000);
+
   clickableImage.classList.add("scaleIt");
-  addEventListener(
-    "animationend",
-    () => {
-      clickableImage.classList.remove("scaleIt");
-    },
-    { once: true }
-  );
+
+  // A handler function for the animationend event
+  function handleAnimationEnd() {
+    clickableImage.classList.remove("scaleIt");
+    clickableImage.removeEventListener("animationend", handleAnimationEnd);
+  }
+
+  // Attach the event listener
+  clickableImage.addEventListener("animationend", handleAnimationEnd, {
+    once: true,
+  });
 });
-
 // for (let i= 0; currentScore)
+let gameState = "david-cameron";
 
-// function scoreEvaluator(x) {
-//   if (x > 0 && x <= 99) {
-//     multiplierValue = multiplierValue * 1;
-//   } else if (x >= 100 && x <= 199) {
-//     multiplierValue = multiplierValue + 2;
-//   } else if (x >= 200 && x <= 299) {
-//     multiplierValue = multiplierValue + 3;
-//   } else if (x >= 300 && x <= 399) {
-//     multiplierValue = multiplierValue + 4;
-//   } else if (x >= 400 && x <= 499) {
-//     multiplierValue = multiplierValue + 5;
-//   } else if (x >= 500 && x <= 599) {
-//     multiplierValue = multiplierValue + 6;
-//   } else if (x >= 600) {
-//     alert("You won the public vote!");
-//   } else if (x <= 0) {
-//     alert("You have faced a parliamentary defeat you weasal!");
-//   }
-// }
+function gameStateCalculator(gameState) {
+  clickableImage.style.backgroundImage = `url(./images/${gameState}.png`;
+}
+// function gameState() {}
 
-// function gameEvaluator(x) {
-//   if (x == 0) {
-//     alert("You have faced a parliamentary defeat you weasal!");
-//   }
-// }
+function gameEvaluator(x) {
+  if (!gameTime == 0) {
+    if (x > 0 && x <= 49) {
+    } else if (x >= 50 && x <= 199) {
+      gameStateCalculator("keir-starmer");
+    } else if (x >= 200 && x <= 299) {
+      gameStateCalculator("rishi-sunak");
+    } else if (x >= 300 && x <= 399) {
+      gameStateCalculator("michael-gove");
+    } else if (x >= 400 && x <= 499) {
+      gameStateCalculator("nigel-farage");
+    } else if (x >= 500 && x <= 599) {
+      gameStateCalculator("theresa-may");
+    } else if (x <= 0) {
+      gameStateCalculator("game-over");
+      rabble.play();
+      alert(
+        "You have faced a parliamentary defeat you weasal! Back to the shadow cabinet for you!"
+      );
+    }
+  } else {
+    gameStateCalculator("game-over");
+    rabble.play();
+    alert("Your time is up!");
+  }
 
-// Game Countdown
-// setTimer(() => {
-//   currentScore = currentScore - 1;
-// }, 10000);
+  // Timer Updater
+  setInterval(() => {
+    gameEvaluator(currentScore);
+    console.log(currentScore);
+    gameTime = gameTime - 1;
+    gameTimeContainer.textContent = `${gameTime}`;
+    innerCountdownBar.setAttribute("style", `width:${gameTime}%`);
+    multiplierContainer.innerHTML = `<p>Public Apathy: ${multiplierValue}</p>`;
+  }, 1000);
+}
 
-// Timer Updater
-setInterval(() => {
-  // gameEvaluator(currentScore);
-  gameTime = gameTime - 1;
-  gameTimeContainer.textContent = `${gameTime}`;
-  innerCountdownBar.setAttribute("style", `width:${gameTime}%`);
-  // reducerContainer.innerHTML = `<p>Crowd Anger: ${reducerValue}</p>`;
-  // multiplierContainer.innerHTML = `<p>Public Apathy: ${multiplierValue}</p>`;
-}, 1000);
-// If score is above level do this..
-// - Update image
-// - Swap old image
-// - colourise/reveal upgrades
-
-// Store history in localstorage
+// To Do
+// ======
+// Local Storage
+// Scoreboard
+//
--- notes.md
+++ notes.md
@@ -8,3 +8,5 @@
 - sound or visual feedback
 - Upgrades
 - High Scores?
+
+The user can visit the page and click the face to increase the score. The timer counts down giving the game an end-point. The user can spend points to increase their multiplier before the time runs out. The user score should be added to the scoreboard and should persist between sessions.
